460a86147754bbb770ed7a53d0ae815e137a558c
Lasse <lassewolffanthony@gmail.com>
2020-05-29 14:51:53 +0200

    Updated PUE to 1.67 as found in 2019 Uptime Institute Annual Data Center Survey

--- carbontracker/constants.py
+++ carbontracker/constants.py
@@ -1,3 +1,3 @@
 # Power Usage Effectiveness Coefficient from
-# Rhonda Ascierto. 2018. Uptime Institute Global Data Center Survey.
-PUE = 1.58
+# Rhonda Ascierto. 2019. Uptime Institute Annual Data Center Survey.
+PUE = 1.67
